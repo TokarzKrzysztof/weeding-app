aa13d41247ea608ea912cfefba329943cfb8381a
TokarzKrzysztof <53493825+TokarzKrzysztof@users.noreply.github.com>
2026-03-12 13:03:25 +0100

    sdsdf

--- frontend/src/pages/Home/contents/czech/ArdspachContent.tsx
+++ frontend/src/pages/Home/contents/czech/ArdspachContent.tsx
@@ -1,5 +1,31 @@
+import { ImagesSet } from 'src/pages/Home/ImagesSet';
+import { Typography } from 'src/ui-components';
+
 export type ArdspachContentProps = {};
 
 export const ArdspachContent = ({ ...props }: ArdspachContentProps) => {
-  return <></>;
+  return (
+    <>
+      <ImagesSet
+        images={['miniaturki/ateny.jpg', 'miniaturki/ateny.jpg', 'miniaturki/ateny.jpg']}
+      />
+      <Typography my={3}>
+        Adršpach to malownicza miejscowość w Czechach, znana przede wszystkim z niezwykłego Skalnego
+        Miasta. Jest to labirynt wysokich, monumentalnych formacji skalnych, które powstały przez
+        tysiące lat w wyniku działania wody i wiatru. Wąskie przejścia, skalne wieże oraz piękne
+        widoki sprawiają, że jest to jedno z najbardziej wyjątkowych miejsc przyrodniczych w tej
+        części Europy. ⛰️
+      </Typography>
+      <ImagesSet
+        images={['miniaturki/ateny.jpg', 'miniaturki/ateny.jpg', 'miniaturki/ateny.jpg']}
+      />
+      <Typography my={3}>
+        Do Adršpachu wybraliśmy się na rodzinny wyjazd. Spacer po Skalnym Mieście był niesamowitym
+        doświadczeniem - ogromne skały i wąskie przejścia między nimi zrobiły na nas naprawdę duże
+        wrażenie.
+        <br /> Momentami czuliśmy się jak w zupełnie innym świecie, a cały spacer był jedną z tych
+        wycieczek, które długo zostają w pamięci. 😊
+      </Typography>
+    </>
+  );
 };
--- frontend/src/pages/Home/contents/turkey/SideContent.tsx
+++ frontend/src/pages/Home/contents/turkey/SideContent.tsx
@@ -1,5 +1,32 @@
+import { ImagesSet } from 'src/pages/Home/ImagesSet';
+import { Typography } from 'src/ui-components';
+
 export type SideContentProps = {};
 
 export const SideContent = ({ ...props }: SideContentProps) => {
-  return <></>;
+  return (
+    <>
+      <ImagesSet
+        images={['miniaturki/ateny.jpg', 'miniaturki/ateny.jpg', 'miniaturki/ateny.jpg']}
+      />
+      <Typography my={3}>
+        Side to popularny kurort w Turcji, położony nad Morzem Śródziemnym. Miejsce słynie z
+        pięknych plaż, ciepłego klimatu oraz starożytnych ruin, które można spotkać niemal na każdym
+        kroku – w tym imponującej świątyni Apollina położonej tuż przy morzu. Miasto łączy wakacyjny
+        wypoczynek z historią sięgającą czasów starożytnej Grecji i Rzymu. 🌅
+      </Typography>
+      <ImagesSet
+        images={['miniaturki/ateny.jpg', 'miniaturki/ateny.jpg', 'miniaturki/ateny.jpg']}
+      />
+      <Typography my={3}>
+        Wyjazd do Side był naszą pierwszą wspólną wycieczką w formule all inclusive. Samo miejsce
+        bardzo nam się podobało - piękna pogoda, atrakcje, ciepłe morze i dużo czasu na odpoczynek sprawiły,
+        że był to przyjemny wyjazd. ☀️
+        <br /> Niestety sam kraj nie przypadł nam aż tak do gustu, głównie ze względu na
+        doświadczenia z lokalnymi sprzedawcami. Na straganach często próbowano nas naciągać lub
+        przekonywać do zakupów w dość nachalny sposób, co trochę popsuło nam ogólne wrażenie z
+        pobytu.
+      </Typography>
+    </>
+  );
 };
