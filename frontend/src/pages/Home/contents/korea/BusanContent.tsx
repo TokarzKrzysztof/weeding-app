b78b7bfd8674d69012295868f6b65a4814f834ca
TokarzKrzysztof <53493825+TokarzKrzysztof@users.noreply.github.com>
2026-05-22 16:44:17 +0200

    korea

--- frontend/src/pages/Home/contents/korea/BusanContent.tsx
+++ frontend/src/pages/Home/contents/korea/BusanContent.tsx
@@ -1,5 +1,29 @@
+import { ImagesSet } from 'src/pages/Trips/ImagesSet';
+import { buildImgSrc, Typography } from 'src/ui-components';
+
 export type BusanContentProps = {};
 
 export const BusanContent = ({ ...props }: BusanContentProps) => {
-  return <></>;
+  return (
+    <>
+      <img src={buildImgSrc('korea/busan-1-1')} alt='' />
+      <Typography my={3}>
+        Busan to drugie co do wielkości miasto Korei Południowej i jeden z najważniejszych portów w
+        kraju. Słynie z pięknych plaż, kolorowych dzielnic, świeżych owoców morza oraz malowniczych
+        świątyń położonych nad morzem. Miasto łączy nowoczesną atmosferę z nadmorskim spokojem i
+        oferuje zupełnie inny klimat niż Seul. To idealne miejsce, by poznać bardziej relaksującą i
+        wakacyjną stronę Korei. 🌊
+      </Typography>
+      <ImagesSet images={['korea/busan-2-1', 'korea/busan-2-2']} />
+      <Typography my={3}>
+        {/* Kioto odwiedziliśmy podczas naszej podróży po Japonii razem z{' '}
+        <DialogRedirectButton miejsce='Tokio'>Tokio</DialogRedirectButton> i{' '}
+        <DialogRedirectButton miejsce='Osaka'>Osaką</DialogRedirectButton>. Miasto od razu zrobiło
+        na nas duże wrażenie swoim spokojem, piękną architekturą i zupełnie inną atmosferą niż
+        nowoczesne Tokio. Spacerując po świątyniach i ogrodach mogliśmy poczuć prawdziwy klimat
+        dawnej Japonii. W Kioto spróbowaliśmy także ramenu ze słynną japońską wołowiną wagyu, a
+        także lokalną tradycyjną kuchnię. */}
+      </Typography>
+    </>
+  );
 };
--- frontend/src/pages/Home/contents/korea/SeulContent.tsx
+++ frontend/src/pages/Home/contents/korea/SeulContent.tsx
@@ -1,5 +1,29 @@
+import { ImagesSet } from 'src/pages/Trips/ImagesSet';
+import { Typography } from 'src/ui-components';
+
 export type SeulContentProps = {};
 
 export const SeulContent = ({ ...props }: SeulContentProps) => {
-  return <></>;
+  return (
+    <>
+      <ImagesSet images={['korea/seul-1-1', 'korea/seul-1-2', 'korea/seul-1-3']} />
+      <Typography my={3}>
+        Seul to stolica Korei Południowej i jedno z najbardziej nowoczesnych miast świata. Słynie z
+        imponujących wieżowców, tętniących życiem ulic, nowoczesnej technologii oraz bogatej kultury
+        K-popu. Jednocześnie można tu znaleźć zabytkowe pałace, tradycyjne świątynie i klimatyczne
+        dzielnice pokazujące historię kraju. Miasto zachwyca energią, różnorodnością i wyjątkowym
+        połączeniem tradycji z nowoczesnością. 🌆
+      </Typography>
+      <ImagesSet images={['korea/seul-2-1', 'korea/seul-2-2']} />
+      <Typography my={3}>
+        {/* Kioto odwiedziliśmy podczas naszej podróży po Japonii razem z{' '}
+        <DialogRedirectButton miejsce='Tokio'>Tokio</DialogRedirectButton> i{' '}
+        <DialogRedirectButton miejsce='Osaka'>Osaką</DialogRedirectButton>. Miasto od razu zrobiło
+        na nas duże wrażenie swoim spokojem, piękną architekturą i zupełnie inną atmosferą niż
+        nowoczesne Tokio. Spacerując po świątyniach i ogrodach mogliśmy poczuć prawdziwy klimat
+        dawnej Japonii. W Kioto spróbowaliśmy także ramenu ze słynną japońską wołowiną wagyu, a
+        także lokalną tradycyjną kuchnię. */}
+      </Typography>
+    </>
+  );
 };
